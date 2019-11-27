4eabcbd0e20f5e78d1dd2d8187c65958b5c79a42
YoanRouleau <yoan.rouleau@etu.univ-smb.fr>
2019-11-27 16:59:58 +0000

    Ajoutez des fichiers projet.

---
+++ Sports Store/Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sports_Store
+{
+    public class Item
+    {
+
+        private int id;
+        private String name;
+        private Double price;
+
+        public Item(int pId, String pName, Double pPrice)
+        {
+            this.Id = pId;
+            this.Name = pName;
+            this.Price = pPrice;
+        }
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+
+    }
+}
